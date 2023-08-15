6b50bd93e830f4a406f10f519291993a98fd6c81
Orlando Javier <oj.flores118@gmail.com>
2023-08-14 21:01:50 -0600

    Compilar archivos con forward, use, as

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require('sass'));
 
 function css( done ) {
 
-    src('src/scss/app.scss')           //Identificr el archivo 
+    src('src/scss/**/*.scss')           //Identificr el archivo 
         .pipe( sass() )                //Compilarlo
         .pipe(dest('build/css'))       //Almacenarla en el disco duro
 
     done();//Callback que avisa a gulp cuando llegamos al final
 }
 function dev ( done ) {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
     done();
 }
 
