db2c1ff68fdcd25a04ac75cd0fe21ca55279e325
Orlando Javier <oj.flores118@gmail.com>
2023-08-29 22:36:52 -0600

    Comenzamos el header con scss

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 
-    src('src/scss/**/*.scss')           //Identificr el archivo 
+    src('src/scss/**/*.scss') //Identificr el archivo 
+        .pipe( plumber() )    //No corta la ejecucion del programa cuando hay algun error    
         .pipe( sass() )                //Compilarlo
         .pipe(dest('build/css'))       //Almacenarla en el disco duro
 
