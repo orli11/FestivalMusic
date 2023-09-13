ad16e0c7c835b821d788f792974c1e301a25e008
Orlando Javier <oj.flores118@gmail.com>
2023-09-12 21:16:42 -0600

    avif y webp

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//Dependencias de CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Dependencias de imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+const cache = require('gulp-cache');
 
 function css( done ) {
 
@@ -11,10 +19,44 @@ function css( done ) {
 
     done();//Callback que avisa a gulp cuando llegamos al final
 }
+function imagenes( done ) {
+    const opciones = {
+        optimizacionLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}') 
+    .pipe( cache(imagemin(opciones)))
+    .pipe( dest('build/img'))
+    done();
+}
+
+function versionAvif ( done ) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}') //Para buscar dos formatos
+    .pipe( avif(opciones) )
+    .pipe( dest('build/img'))
+
+    done();
+}
+function versionWebp ( done ) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png, jpg}') //Para buscar dos formatos
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img'))
+
+    done();
+}
+
 function dev ( done ) {
     watch('src/scss/**/*.scss', css)
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
